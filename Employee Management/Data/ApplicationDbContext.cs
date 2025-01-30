c6c2827a2bb6abdd088bbbbf659edcf86fdbf319
nzommmo <110990728+nzommmo@users.noreply.github.com>
2025-01-31 00:03:24 +0300

    Update

---
+++ Employee Management/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Employee_Management.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee_Management.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<Employee> Employees {get; set;}
+}
